c76026ee4b3fb4c50e1ad59eb6cc06dc5090d115
Artem <artmalini@gmail.com>
2017-12-20 20:33:10 +0200

    v5

---
+++ temp/ft_printf.h
@@ -0,0 +1,50 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdlib.h>
+# include <stdarg.h>
+# include <float.h>
+//# include "../libft/libft.h"
+# define MAX(a, b) (a > b ? a : b)
+
+typedef struct		s_placehold
+{
+	int				leftalign;
+	char			sign;
+	char			padding;
+	char			*hash;
+	int				width;
+	int				prec;
+	char			*length;
+	char			type;
+	unsigned short	base;
+	int				signed_num;
+	unsigned short	uppercase;
+	int				sigfig;
+	short			exp_base;
+	char			exp_char;
+	short			exp_len;
+}					t_placehold;
+
+int					ft_printf(const char *format, ...);
+int					ft_vfdprintf(int fd, const char *format, va_list a_list);
+int					ft_fdprintf(int fd, const char *format, ...);
+void				set_hash(t_placehold *p, const char *e);
+void				eval_fields(t_placehold *p, const char **e, va_list a_list);
+size_t				print_eval(int fd, t_placehold *p, va_list a_list,
+						size_t cnt);
+void				set_type_field(t_placehold *p, const char *e);
+void				set_flag_field(t_placehold *p, const char **e);
+void				set_width_field(t_placehold *p, const char **e,
+						va_list a_list);
+void				set_precision_field(t_placehold *p, const char **e,
+						va_list a_list);
+void				set_length_field(t_placehold *p, const char **e);
+char				*ft_uitoa_base(uintmax_t value, unsigned short base,
+						unsigned short uppercase, int digits);
+char				*ft_printf_itoa_base(t_placehold *p, va_list a_list);
+char				*ft_printf_ctos(t_placehold *p, va_list a_list);
+char				*ft_printf_str(t_placehold *p, size_t n, va_list a_list);
+char				*ft_printf_ftoa(t_placehold *p, va_list a_list);
+long double			ft_uld_get_mantissa(long double ld, short base);
+
+#endif
