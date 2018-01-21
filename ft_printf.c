f5287f7d61281469bf9a4f37a2038cbe153970b8
Artem <artmalini@gmail.com>
2018-01-21 13:21:32 +0200

    d15

--- ft_printf.c
+++ ft_printf.c
@@ -1572,7 +1572,7 @@ int 	ft_printf(const char *format, ...)
 	return (tick);
 }
 
-int 		main(void)
+/*int 		main(void)
 {
 	int i, j;
 	char c;
@@ -1725,5 +1725,5 @@ int 		main(void)
 	//ft_printf("%#b\n", 128);
 
 	return (0);
-}
+}*/
 
